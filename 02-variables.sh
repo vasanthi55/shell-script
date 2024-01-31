8505009cbbb62ff700f163595c53dea685273530
vasanthi55 <vasanthi.ankem@gmail.com>
2024-01-31 14:13:31 +0530

    added variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh:hey suresh GM"
-echo "Suresh:hi ramesh VGM"
-echo "Ramesh:wat dng"
-echo "Suresh:learning aws"
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1:hey $PERSON2 GM"
+echo "$PERSON2:hi $PERSON1 VGM"
+echo "$PERSON1:wat dng"
+echo "$PERSON2:learning aws"
