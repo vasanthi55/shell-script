7642150e9e7d5cbd9ea8332a37a81cf16e11d493
vasanthi55 <vasanthi.ankem@gmail.com>
2024-01-31 17:10:12 +0530

    Using array for multiple items

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -7,3 +7,11 @@ echo "first fruit: ${FRUITS[0]}"
 echo "second fruit: ${FRUITS[1]}"
 
 echo "third fruit: ${FRUITS[2]}"
+
+#when enter multiple item can create only one variable and 
+#can given entries as showm above using array
+#${FRUITS[1]}--this syntax is important to print
+
+echo "ALL fruits : ${FRUITS[@]}"
+
+#if need all items at a time can use '@' as given above in index place
