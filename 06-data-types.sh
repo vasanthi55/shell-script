1ed7d7f8be00cd448f0503a6d39bb84bd0ac08e6
vasanthi55 <vasanthi.ankem@gmail.com>
2024-01-31 16:46:03 +0530

    shell consider datatypes automatically

---
+++ 06-data-types.sh
@@ -0,0 +1,14 @@
+#!bin/bash
+
+NUMBER=$1
+NUMBER=$2
+
+#shell automatically consider data types 
+#no need to specify particulary whther it is int or any other
+#if enter text instead of number while dng sum it gives  zero value
+
+SUM=($NUMBER1+$NUMBER2)
+
+echo "Total value : $SUM"
+
+#here taking another variable SUM and adding numbers
