3dc3cc8dc4cb89a77d69276cfe5f785dfa87c908
vasanthi55 <vasanthi.ankem@gmail.com>
2024-01-31 15:53:53 +0530

    checking

--- 05-variables-read.sh
+++ 05-variables-read.sh
@@ -2,5 +2,5 @@
 
 echo "Please enter username"
 
-read USERNAME # the value entered by user is automatically attached to USERNAME variable
+read -s USERNAME # the value entered by user is automatically attached to USERNAME variable
 
