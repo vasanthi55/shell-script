10e19a80770376b43220a10e2c8726996d825485
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-01 13:55:33 +0530

    if condition

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-NUMBER1=$1
+NUMBER=$1
 
 if [ $NUMBER -gt 100 ]
 then
-   echo "$NUMBER is greater than 100"
+   echo " Given $NUMBER is greater than 100"
 else
-   echo "$NUMBER is not greater than 100"
+   echo "Given $NUMBER is not greater than 100"
 fi
