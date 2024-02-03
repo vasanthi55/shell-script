d5d904ba8833a817f90c9f7e5ec04b3e29d3fcad
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-03 15:34:20 +0530

    using function installing packages without repeating same code

--- 10-functions.sh
+++ 10-functions.sh
@@ -4,13 +4,17 @@ ID=$(id -u)
 
 VALIDATE(){ #function name is validate here.WHich one we think the code repeating that one wil put under function
             #Usually below of the variables we will declare function
-    if [ $? -ne 0 ]
+    if [ $1 -ne 0 ]
 then 
-  echo "ERROR: installing  failed"
+  echo "ERROR:  $2 installing  failed"
   exit 1
 else
-  echo "Installing success"
+  echo " $2 Installing success"
 fi
+
+#Above We are giving $1, $2
+# $1 will check to the $? the xit status if not zero faile.
+#$2 check exit status  below and give success
 
     
 }
@@ -25,10 +29,11 @@ fi
 
 yum install mysql -y
 
-VALIDATE
+VALIDATE $? " installed mysql" #passing argument $? that gives the mysql exit status and to show what we did like mysql installed
 
 yum install git -y
 
-VALIDATE
+VALIDATE $? " installed git"
 
 # we have called function in starting to work that here last we call the function name to work
+#it is very imp to give some inputs. to functions also we give inputs through arguments
