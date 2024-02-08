098324e2705079b57505e540beca9cc8350a4955
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-08 14:53:19 +0530

    DELETING OLD LOGS more than 14days

---
+++ 13-delete-old-logs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SOURCE_DIR="/tmp/shellscript-logs"
+
+if [ ! -d $SOURCE_DIR ] # ! - denotes opposite, here if source dir doesnt exit 
+then    
+    echo -e "$R Source directory: $SOURCE_DIR not exits. $N"
+fi
+
+#in the directory find for files older than 14 i.e only log file extensions.
+FILES_To_DELETE=$(find $SOURCE_DIR -type f -mtime +14 -name "*.log") 
+
+while IFS=read -r line #IFS=internal field seperator-read line by line
+do
+    echo "Deleting files:$line"
+    
+    rm-rf $line
+
+done <<< $FILES_TO_DELETE
+
+#without while lop also we can do by giving exec rm -rf to the above command.
+#<<<=giving input the while loop from files_to_delete command
