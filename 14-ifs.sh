0100b5a332316d07e1aee5d95f87518c68d0698a
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-08 16:47:43 +0530

    reading fields of passwd file using while loop

--- 14-ifs.sh
+++ 14-ifs.sh
@@ -10,9 +10,11 @@ then
 fi
 
 #reading passwd file fields using delimeter or internal field seperator with while loop prnting the output
-while IFS=":"  read -r username password user_id group_id
+while IFS=":"  read -r username password user_id group_id user_fullname
 do
     echo "username: $username"
     echo "user_id: $user_id"
     echo "group_id: $group_id"
+    echo "user_fullname: $user_fullname"
+    
 done < $file
