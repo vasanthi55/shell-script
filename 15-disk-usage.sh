7f9201209d8d9c3dd09864b5154ec922cc2648f8
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-09 16:21:02 +0530

    Using while loop setting disk usage alert

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DISK_USAGE=$(dt -hT | grep -vE 'tmp|File') #except temp or filesystem give other files means disk
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File') #except temp or filesystem give other files means disk
 DISK_THRESHOLD=1 
 message=""
 
@@ -12,11 +12,16 @@ do
     usage=$(echo $line | awk '{print 6F}' | cut -d % -f1)
     partition=$(echo $line | awk '{print 1F}')
     if [ usage -ge $DISK_THRESHOLD ]
-    message+="High disk usage on $partition: $usage"
+    then
+        message+="High disk usage on $partition: $usage\n"
+    fi
 
 done <<< $DISK_USAGE
+
+echo -e "Message:$message"
 
 #11th line-->$line=go line by libe go to fist line of the disk usage by cutting percentage symbol
 #12-->go to first line and give file to which it belongs to disk name and then 2nd one
 
 #"+="-->to not overwrite the second line .when second line comes it comes down not in the same line
+#+\n -->second line comes in next line
