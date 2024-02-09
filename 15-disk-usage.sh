1d91758d1e5fdf105c2d62b4aae15b9f78c76019
vasanthi55 <vasanthi.ankem@gmail.com>
2024-02-09 16:23:56 +0530

    Using while loop setting disk usage alert

--- 15-disk-usage.sh
+++ 15-disk-usage.sh
@@ -9,8 +9,8 @@ message=""
 
 while IFS= read line
 do
-    usage=$(echo $line | awk '{print 6F}' | cut -d % -f1)
-    partition=$(echo $line | awk '{print 1F}')
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    partition=$(echo $line | awk '{print $1F}')
     if [ $usage -ge $DISK_THRESHOLD ]
     then
         message+="High disk usage on $partition: $usage\n"
