16459da1a8b160b35e38942f6f085cb36d3f4480
vasanthi55 <vasanthi.ankem@gmail.com>
2024-03-13 16:59:06 +0530

    aws resoirce usage tracker with cronjob

---
+++ 18-awsproject-resource-tracker.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+##################
+
+# Author : Vasanthi
+# Date : 13th march
+#version : v1
+
+# this script will report aws resources usage
+# AWS ec2
+# aws s3
+# aws lambda
+# aws IAM users
+# refer aws cli reference for all resource to list or launch or terminate.
+
+# listing s3 buckets 
+
+aws s3 ls
+
+# listing ec2 instances
+
+aws ec2 describe-instances
+
+# listing lambda
+
+aws lambda list-functions
+
+# listing IAM users
+
+aws iam list-users
